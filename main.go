9ef40fbaf68e746786bb61026ab2d8ae020c1dd0
syrex1013 <31669127+syrex1013@users.noreply.github.com>
2021-06-05 14:53:35 +0200

    Update main.go

--- main.go
+++ main.go
@@ -5,13 +5,6 @@ import (
 	"os"
 )
 
-func RUNPE() {
-	if len(os.Args) < 4 {
-		fmt.Println("Please input payload and target path!")
-		os.Exit(1)
-	}
-	payloadPath := os.Args[1]
-	targetPath := os.Args[2]
-	arguments := os.Args[3]
+func RUNPE(payloadPath string, targetPath string, arguments string) {
 	HollowProcess(payloadPath, targetPath, arguments)
 }
