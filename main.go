67be442cfb78c7b5a1c05dd32a84ab254b233ee3
syrex1013 <31669127+syrex1013@users.noreply.github.com>
2021-06-27 17:22:30 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package RUNPE
 
-func RUNPE(payloadPath string, targetPath string, arguments string, payloadByte []byte) {
-	HollowProcess(payloadPath, targetPath, arguments,payloadByte)
+func RUNPE(payloadPath string, targetPath string, arguments string, payloadByte []byte) int {
+	pid := 0
+	_,pid = HollowProcess(payloadPath, targetPath, arguments,payloadByte)
+	return pid
 }
