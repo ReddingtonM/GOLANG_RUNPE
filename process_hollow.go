8768f5e82b605266458827e1ced1e893283b878d
syrex1013 <31669127+syrex1013@users.noreply.github.com>
2021-07-20 11:21:28 +0200

    Update process_hollow.go

--- process_hollow.go
+++ process_hollow.go
@@ -14,7 +14,6 @@ func HollowProcess(payloadPath, targetPath string, arguments string, payloadByte
 	//1. Load the payload:
 	var payloadImageSize uint64
 	// Load the current executable from the file with the help of libpeconv:
-	loadedPE := nil
 	if payloadPath == "" {
 		loadedPE = LoadPeModuleInMemory(payloadByte, &payloadImageSize)
 	} else {
